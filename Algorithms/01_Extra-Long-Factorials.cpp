dc580abb86dc63c03c5dbec5469ccf7cdfc44a10
Akiko Kawai <haruka.sao@gmail.com>
2022-03-23 11:58:33 +0900

    WIP

---
+++ Algorithms/01_Extra-Long-Factorials.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+void large_pattern(int n) {
+}
+
+void extraLongFactorials(int n) {
+    if(n > 20){
+        large_pattern(n);
+        return;
+    }
+    
+    unsigned int sum = 1;
+    for(int i = 1; i < (n+1); ++i){
+        sum *= i;
+    }
+    std::cout << sum;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    extraLongFactorials(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
